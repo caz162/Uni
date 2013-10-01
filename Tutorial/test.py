d422f3180d0102884dc7439a6b0ea3ea4ebc111d
unknown <vsdev1@ME0B056-01205A.c27-merch.napier.ac.uk>
2013-10-01 12:25:35 +0100

    Attempting to add database support. Accidentally removed dll's from python folder going to restart pc
    Signed-off-by: unknown <vsdev1@ME0B056-01205A.c27-merch.napier.ac.uk>

--- Tutorial/test.py
+++ Tutorial/test.py
@@ -1,11 +1,14 @@
 import web, os
+import sqlite3
 from web import form
 
-
+USER_DATA_DIR = "userData"
 render = web.template.render('templates/')
 
+db = web.database(dbn='sqlite', db='testdb.sqlite')
+
 urls = ('/', 'index')
-app = web.application(urls, globals())
+
 
 myform = form.Form( 
     form.Textbox("name"), 
@@ -16,12 +19,17 @@ myform = form.Form(
     form.Dropdown('difficulty', ['All_Mountain', 'All_Mountain_Plus', 'XC']),
     form.Dropdown('terrain', ['Rock', 'Grass', 'Road']))
 
+def loadDB(name):
+    return web.database(dbn="sqlite", db=os.path.join(USER_DATA_DIR,"{}.sqlite".format(name)))
+
+
 class index: 
     def GET(self): 
         form = myform()
-        # make sure you create a copy of the form by calling it (line above)
-        # Otherwise changes will appear globally
+
         print "hello"
+        todos = db.select('todo')
+        #return render.index('todo')
         return render.formtest(form)
 
     def POST(self): 
@@ -29,10 +37,30 @@ class index:
         if not form.validates(): 
             return render.formtest(form)
         else:
-            # form.d.boe and form['boe'].value are equivalent ways of
-            # extracting the validated arguments from the form.
+
             return "Worked name: %s, pref1: %s, pref2: %s, pref3: %s, difficulty: %s, terrain: %s" % (form.d.name,form['pref1'].checked,form['pref2'].checked,form['pref3'].checked, form['difficulty'].value, form.d.terrain)
 
 if __name__=="__main__":
+    if not os.path.exists(USER_DATA_DIR) and not os.path.isdir(USER_DATA_DIR):
+        os.mkdir(USER_DATA_DIR)
+    if not os.path.exists(os.path.join(USER_DATA_DIR,"main.sqlite")):
+        db = loadDB("main")
+
+        db.query("CREATE TABLE PointType(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+
+
+        #db.query("INSERT INTO PointType(name, icon) VALUES ('{}', x'{}')".format(n,d))
+
+        # Points table represents knowledge about an area such as an eatery, a good sight or a rest spot.
+        db.query("""CREATE TABLE Point(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, desc TEXT, lat NUMERIC, lon NUMERIC, type INTEGER REFERENCES pointType(id))""")
+
+        # Route is just here to help maintain a list of routes
+        db.query("""CREATE TABLE Route(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)""")
+
+        # RoutePoint joins route and point together
+        db.query("""CREATE TABLE RoutePoint(route_id INTEGER REFERENCES Route(id), point_id INTEGER REFERENCES Point(id), PRIMARY KEY (route_id, point_id))""")
+
+        
     web.internalerror = web.debugerror
+    app = web.application(urls, globals())
     app.run()
